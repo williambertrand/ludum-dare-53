a40cdaa48ca22e4ba1867b0fd59d2df7edea9fdc
GrantHallDevelopment <Jordan.grant-hall@OnTheblockgaming.app>
2023-04-29 20:37:24 +0300

    Spawn System added

---
+++ Assets/DamageData.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class DamageData
+{
+    public Transform damageDealer;
+    public Transform target;
+    public float damageDealt;
+
+    /// <summary>
+    /// This will return 1 if the target is right, and -1 for left of the damageDealer
+    /// </summary>
+    /// <returns></returns>
+    public int GetDirectionToTarget()
+    {
+        Vector3 direction = damageDealer.position - damageDealer.position;
+        return (int)Mathf.Sign(direction.x);
+    }
+}
---
+++ Assets/Scripts/Gameplay/Spawning/SpawnPoint.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    public LayerMask checkLayers;
+    public float detectionRange;
+    public Entity SpawnEntity(Entity entity)
+    {
+        Entity spawnedEnemy = CreateEntity(entity);
+        return spawnedEnemy;
+    }
+
+    public Entity CreateEntity(Entity entityToSpawn)
+    {
+        return Instantiate(entityToSpawn, transform.position, Quaternion.identity);
+    }
+
+    public bool CanSpawn()
+    {
+        return Physics2D.OverlapCircle(transform.position, detectionRange, checkLayers) == null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, detectionRange);
+    }
+}
